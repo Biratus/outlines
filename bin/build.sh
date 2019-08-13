940eb3811952e25dfc3fcaec00a644cf908eb05b
Biratus <cbirette@iocean.fr>
2019-08-13 14:05:39 +0200

    set build dir to dist

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Make a copy of the src files for use on the github pages
-echo "   Copying src to docs/dist..."
-rm -rf docs/dist
-mkdir -p docs/dist
-cp src/outlines.js docs/dist/
-cp src/Unistroke.js docs/dist/
+echo "   Copying src to /dist..."
+rm -rf dist
+mkdir -p dist
+cp src/outlines.js dist/
+cp src/Unistroke.js dist/
 echo "   Done."
